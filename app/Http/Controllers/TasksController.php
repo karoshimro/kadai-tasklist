e6654b87553d4e734f9eb765e76170fd34ed212c
EC2 Default User <ec2-user@ip-172-31-0-231.us-east-2.compute.internal>
2018-05-29 01:51:38 +0000

    complete

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,7 +44,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+         $this->validate($request, [
+              'status' => 'required|max:10', 
+            'content' => 'required|max:191',
+        ]);
           $task = new Task;
+           $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -89,8 +94,13 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
+    {  $this->validate($request, [
+        'status' => 'required|max:10', 
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
+         $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
