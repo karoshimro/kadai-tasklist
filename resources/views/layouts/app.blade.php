1c1adb86d71b16b2b85f3277107c1932e14c6a24
EC2 Default User <ec2-user@ip-172-31-0-231.us-east-2.compute.internal>
2018-05-31 04:29:11 +0000

    bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,29 @@
 <html>
     <head>
         <meta charset="utf-8">
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>TaskList</title>
+        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
 
     <body>
-        
-        @include('commons.error_messages')
-        @yield('content')
+       @include('commons.navbar')
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+
+            @include('commons.error_messages')
+            
+            @yield('content')
+                </div>
+            </div>
+        </div>
+           
     </body>
 </html>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,20 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
+<!--<div class="row">-->
+<!--  <div class="col-xs-12　col-sm-8 col-sm-offset-2　col-md-8 col-md-offset-2　col-lg-6 col-lg-offset-3">-->
 
   <h1>タスク新規作成ページ</h1>
 
    
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-     {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
-
+            
+             <!--<div class="col-xs-12">-->
+             <!--   <div class="col-sm-8 col-sm-offset-2">-->
+             <!--       <div class="col-md-8 col-md-offset-2">-->
+             <!--           <div class="col-lg-6 col-lg-offset-3">-->
+                          
+                            <div class="form-group">
+                              
+                                {!! Form::label('status', 'ステータス:') !!}
+                                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                            </div>
+                                
+                            <div class="form-group">
+                                {!! Form::label('content', 'タスク:') !!}
+                                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                            </div>
+                       
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+ 
+            <!--</div>-->
+            <!--</div>-->
 @endsection
